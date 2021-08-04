c70a19bb4cf528f1fcc6985e85f523e3c21c9341
Vertib <vertibs@gmail.com>
2021-08-04 17:09:27 +0300

    Initial Commit

---
+++ My Card App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My Card App
+//
+//  Created by Nikita Aksenkin on 04/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Card App/My_Card_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Card_AppApp.swift
+//  My Card App
+//
+//  Created by Nikita Aksenkin on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct My_Card_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
