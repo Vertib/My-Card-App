5f6a04370b8ee8672b31ff3441a4c874e7f9daa8
Vertib <vertibs@gmail.com>
2021-08-04 20:15:49 +0300

    First App

--- My Card App/ContentView.swift
+++ My Card App/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.96, green: 0.80, blue: 0.47).ignoresSafeArea()
+            VStack {
+                Image("Photo").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/).frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/
+                    .stroke(lineWidth: 5))
+                Text("Аксенкин Никита")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                Text("IOS Developer / Student BMSTU")
+                    .font(.headline)
+                    .fontWeight(.light)
+                Card(image: "phone.fill", message: "+79999999999")
+                Card(image: "envelope.fill", message: "vertibs@gmail.com")
+
+            }.foregroundColor(.white)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
+                         struct Card: View {
+                            let image: String
+                            let message: String
+                            var body: some View {
+                                RoundedRectangle(cornerRadius: 25).padding(.horizontal).frame(height: 50).overlay(HStack {
+                                    Image(systemName: image)
+                                    Text(message)
+                                        .fontWeight(.semibold)
+                                }.foregroundColor(Color(red: 0.96, green: 0.80, blue: 0.47)))
+                            }
+                         }
+                         
